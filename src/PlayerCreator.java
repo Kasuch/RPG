bc5c7fac344142bede9bd8d71efbf24bb9dd8618
Kasuch <ksuchanowski@gmail.com>
2022-03-07 15:27:03 +0100

    Initial commit

---
+++ src/PlayerCreator.java
@@ -0,0 +1,9 @@
+public class PlayerCreator {
+    private String name;
+    private int strength;
+    private int dexterity;
+    private int intelligence;
+    private int vitality;
+    private int health;
+
+}
