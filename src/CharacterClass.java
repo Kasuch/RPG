63204ade41eef6d644e20216d3a791f294bf6265
Kasuch <ksuchanowski@gmail.com>
2022-03-09 14:55:33 +0100

    Updating CharacterClass to allow user selection using numbers; Adding initial game steps in main (player creation)

--- src/CharacterClass.java
+++ src/CharacterClass.java
@@ -3,20 +3,22 @@ Define available character classes and their base attributes
  */
 
 public enum CharacterClass {
-    WARRIOR(30, 20, 10, 20),
-    WIZARD(10, 20, 30, 20),
-    ASSASSIN(20, 30, 10,20);
+    WARRIOR(30, 20, 10, 20, 1),
+    WIZARD(10, 20, 30, 20, 2),
+    ASSASSIN(20, 30, 10,20, 3);
 
     private int strength;
     private int dexterity;
     private int intelligence;
     private int vitality;
+    private int selection;
 
-    CharacterClass(int strength, int dexterity, int intelligence, int vitality) {
+    CharacterClass(int strength, int dexterity, int intelligence, int vitality, int selection) {
         this.strength = strength;
         this.dexterity = dexterity;
         this.intelligence = intelligence;
         this.vitality = vitality;
+        this.selection = selection;
     }
 
     public int getStrength() {
@@ -49,5 +51,14 @@ public enum CharacterClass {
 
     public void setVitality(int vitality) {
         this.vitality = vitality;
+    }
+
+    public static CharacterClass getCharacterClass(int selection) {
+        for (CharacterClass element : values()) {
+            if (selection == element.selection) {
+                return element;
+            }
+        }
+        return null;
     }
 }
--- src/RPGMain.java
+++ src/RPGMain.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class RPGMain {
 
     public static void main(String[] args) {
-        PlayerCreator player = new PlayerCreator("Mighty John", CharacterClass.WARRIOR);
+        Scanner textScanner = new Scanner(System.in);
+        Scanner intScanner = new Scanner(System.in);
+        CharacterClass[] characterClasses = CharacterClass.values();
+        int selectedClass;
+        System.out.println("Welcome to awesome text RPG adventure!");
+        System.out.println("What is your name?");
+        String playerName = textScanner.nextLine();
+
+        do {
+            System.out.println("Select your class:");
+            for (int i = 0; i < characterClasses.length; i++) {
+                System.out.println("[" + (i + 1) + "] " + characterClasses[i]);
+            }
+            selectedClass = intScanner.nextInt();
+        } while (selectedClass < 1 || selectedClass > characterClasses.length);
+
+
+        PlayerCreator player = new PlayerCreator(playerName, CharacterClass.getCharacterClass(selectedClass));
         System.out.println(player);
-        Dice dice = new Dice();
-        System.out.println(dice.roll(4));
+//        Dice dice = new Dice();
+//        System.out.println(dice.roll(4));
     }
 }
